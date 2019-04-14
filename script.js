0a4560c85ca326dde878219edff28a5f01d0f0ff
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-04-14 16:24:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-window.onscroll = function() {stickyFunction()};
-		
-var navbar = document.getElementById("myTopnav");
-var sticky = navbar.offsetTop;
-
-function stickyFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
