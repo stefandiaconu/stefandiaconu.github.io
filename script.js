49e9f5e655d67a1cfa68a1ceac99906635df1d51
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-04-14 14:06:36 +0100

    Create sticky navbar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+window.onscroll = function() {stickyFunction()};
+		
+var header = document.getElementById("myTopnav");
+var sticky = header.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
