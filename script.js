8ebf86b99af6ea7ea545841e74b4143ed158615d
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-04-15 23:34:39 +0100

    Redesign university section

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ function myFunction() {
       x.className = "topnav";
     }
 }
+
+var y1 = document.getElementById("yearone")
+var y2 = document.getElementById("yeartwo")
+var y3 = document.getElementById("yearthree")
+
+function year1Function() {
+  var x = document.getElementById("firstyear");
+
+  if (x.className === "firstyear1") {
+    console.log("Hello, Year 1!");
+    if (y1.className === "yearone1") {
+      y1.style.display = "block";
+      y2.style.display = "none";
+      y3.style.display = "none";
+    }
+  }
+}
+
+function year2Function() {
+  var x = document.getElementById("secondyear");
+
+  if (x.className === "secondyear2") {
+    console.log("Hello, Year 2!");
+    if (y2.className === "yeartwo2") {
+      y1.style.display = "none";
+      y2.style.display = "block";
+      y3.style.display = "none";
+    }
+  }
+}
+
+function year3Function() {
+  var x = document.getElementById("thirdyear");
+
+  if (x.className === "thirdyear3") {
+    console.log("Hello, Year 3!");
+    if (y3.className === "yearthree3") {
+      y1.style.display = "none";
+      y2.style.display = "none";
+      y3.style.display = "block";
+    }
+  }
+}
